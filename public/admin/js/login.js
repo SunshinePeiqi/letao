4f5fca6f986e5243ed6c03ce47a38a10306adae1
SunshinePeiqi <514199708@qq.com>
2019-03-20 15:20:04 +0800

    项目初始化

---
+++ public/admin/js/login.js
@@ -0,0 +1,85 @@
+
+
+$(function () {
+
+    // alert('1')
+    // 表单校验
+    $('#form').bootstrapValidator({
+        fields: {
+            //校验用户名，对应name表单的name属性
+            username: {
+                validators: {
+                    //不能为空
+                    notEmpty: {
+                        message: '用户名不能为空'
+                    },
+                    //长度校验
+                    stringLength: {
+                        min: 3,
+                        max: 9,
+                        message: '用户名长度必须在3到9之间'
+                    },
+                    //正则校验
+                    regexp: {
+                        regexp: /^[a-zA-Z0-9_\.]+$/,
+                        message: '用户名由数字字母下划线和.组成'
+                    }
+                }
+            },
+            // 校验密码
+            password: {
+                validators: {
+
+                    // 不能为空
+                    notEmpty: {
+                        message: '密码不能为空'
+                    },
+                    // 长度校验
+                    stringLength: {
+                        min: 6,
+                        max: 12,
+                        message: '用户名密码逆序在6到12位'
+                    }
+                }
+            },
+            
+
+        },
+
+        // 校验时的图标
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-thumbs-up',
+            invalid: 'glyphicon glyphicon-thumbs-down',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+    })
+
+
+    // 发送ajax请求
+    $("#form").on('success.form.bv', function (e) {
+        e.preventDefault();
+        //使用ajax提交逻辑
+        // alert('1');
+        $.ajax({
+            type: 'post',
+            url: '/employee/employeeLogin',            
+            data: $("#form").serialize(),
+            success: function (info) {
+                console.log(info);
+                if (info.error === 1000) {
+                    alert('用户名不存在')
+                }
+                if (info.error === 1001) {
+                    alert('用户名密码错误')
+                }
+                if (info.success) {
+                    location.href = 'index.html'
+                }
+            }
+        })
+    });
+
+
+
+
+})
