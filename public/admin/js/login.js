64db1f831fe13e547c712778de3404cbcdc8ee3a
SunshinePeiqi <514199708@qq.com>
2019-03-20 16:17:15 +0800

    登录页面完成

--- public/admin/js/login.js
+++ public/admin/js/login.js
@@ -23,6 +23,9 @@ $(function () {
                     regexp: {
                         regexp: /^[a-zA-Z0-9_\.]+$/,
                         message: '用户名由数字字母下划线和.组成'
+                    },
+                    callback: {
+                        message: '用户名错误'
                     }
                 }
             },
@@ -39,6 +42,9 @@ $(function () {
                         min: 6,
                         max: 12,
                         message: '用户名密码逆序在6到12位'
+                    },
+                    callback: {
+                        message: '密码错误'
                     }
                 }
             },
@@ -55,7 +61,7 @@ $(function () {
     })
 
 
-    // 发送ajax请求
+    // 点击登录按钮 发送ajax请求
     $("#form").on('success.form.bv', function (e) {
         e.preventDefault();
         //使用ajax提交逻辑
@@ -67,10 +73,15 @@ $(function () {
             success: function (info) {
                 console.log(info);
                 if (info.error === 1000) {
-                    alert('用户名不存在')
+                    // alert('用户名不存在')
+                    // 参数1 : 修改哪个字段
+                    // 参数2 : 修改的状态
+                    // 参数3 ：指定显示哪个错误的信息
+                    $('#form').data('bootstrapValidator').updateStatus('username', 'INVALID', 'callback')
                 }
                 if (info.error === 1001) {
-                    alert('用户名密码错误')
+                    // alert('用户名密码错误')
+                    $('#form').data('bootstrapValidator').updateStatus('password', 'INVALID', 'callback')
                 }
                 if (info.success) {
                     location.href = 'index.html'
@@ -79,6 +90,22 @@ $(function () {
         })
     });
 
+
+    // 表单重置
+    $('[type=reset]').on('click', function () {
+        $('#form').data('bootstrapValidator').resetForm(true);
+    })
+
+    // ajax请求全局事件  显示进度条
+    $(document).ajaxStart(function () {
+        // 开始发送请求
+        NProgress.start();
+    });
+
+    $(document).ajaxStop(function () {
+        // 结束发送请求
+        NProgress.done();
+    })
 
 
 
